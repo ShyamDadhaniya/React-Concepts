852a2f7f2f6478f8e7364eaad8f182491c731ef0
SHYAM DADHANIYA <74183113+ShyamDadhaniya@users.noreply.github.com>
2022-04-12 23:00:49 +0530

    Class Component Example

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+import { Component } from "react/cjs/react.production.min";
+import classes from "./User.module.css";
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+export default User;
