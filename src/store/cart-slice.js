fcc0a28ce8010a4b76178939db22528700bef547
SHYAM DADHANIYA <74183113+ShyamDadhaniya@users.noreply.github.com>
2022-04-12 20:49:09 +0530

    Advance Redux Cart-App

---
+++ src/store/cart-slice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    item: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.item.find((item) => item.itemId === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.item.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      console.log("id:-" +id)
+      const existingItem = state.item.find((item) => item.itemId === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.item = state.item.filter((item) => item.itemId  !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+export const cartActions = cartSlice.actions;
+export default cartSlice;
